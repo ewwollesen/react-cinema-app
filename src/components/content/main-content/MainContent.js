d7b0b55d8ec4051e296d1b661577ec797da7ad5f
Wayne Wollesen <wayniackc@gmail.com>
2021-07-06 17:43:00 -0500

    created pagination feature

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import SlideShow from '../slide-show/SlideShow';
+import React, { useState } from 'react';
 
 import './MainContent.scss';
+import SlideShow from '../slide-show/SlideShow';
+import Paginate from '../paginate/Paginate';
 
 const MainContent = () => {
   const images = [
@@ -15,12 +16,24 @@ const MainContent = () => {
       url: 'https://images.pexels.com/photos/2577848/pexels-photo-2577848.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     }
   ];
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage >= 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="main-content">
       <SlideShow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movie-type">Now Playing</div>
-        <div className="paginate">Paginate</div>
+        <div className="paginate">
+          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+        </div>
       </div>
       {/* display grid component */}
     </div>
