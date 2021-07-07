8969e8387ec98d92fb60f8702df651e938bbbe3d
Wayne Wollesen <wayniackc@gmail.com>
2021-07-06 21:26:05 -0500

    created grid component and star props

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -3,17 +3,33 @@ import React, { useState } from 'react';
 import './MainContent.scss';
 import SlideShow from '../slide-show/SlideShow';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
   const images = [
     {
-      url: 'https://images.pexels.com/photos/6134742/pexels-photo-6134742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      url: 'https://images.pexels.com/photos/6134742/pexels-photo-6134742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.5
     },
     {
-      url: 'https://images.pexels.com/photos/8497533/pexels-photo-8497533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      url: 'https://images.pexels.com/photos/6134742/pexels-photo-6134742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5
     },
     {
-      url: 'https://images.pexels.com/photos/2577848/pexels-photo-2577848.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      url: 'https://images.pexels.com/photos/8497533/pexels-photo-8497533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      rating: 7.8
+    },
+    {
+      url: 'https://images.pexels.com/photos/8497533/pexels-photo-8497533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      rating: 9.7
+    },
+    {
+      url: 'https://images.pexels.com/photos/2577848/pexels-photo-2577848.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      rating: 6.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/2577848/pexels-photo-2577848.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      rating: 8.5
     }
   ];
   const [currentPage, setCurrentPage] = useState(1);
@@ -35,7 +51,7 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      {/* display grid component */}
+      <Grid images={images} />
     </div>
   );
 };
